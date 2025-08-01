d0d2b12942c9bb0b41628294fefb776e2c914da8
shuna421 <shuna2001@icloud.com>
2025-08-01 20:35:08 +0900

    課題提出用: 投稿一覧画面表示の実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+        $posts = DB::table('posts')->get();
+        return view('posts.index', ['posts' => $posts]);
     }
+    
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,19 @@
-<!-- resources/views/posts/index.blade.php -->
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
+    <meta charset="UTF-8">
     <title>投稿一覧</title>
 </head>
 <body>
     <h1>投稿一覧</h1>
+
+    <ul>
+        @foreach ($posts as $post)
+            <li>
+                <strong>{{ $post->title }}</strong><br>
+                {{ $post->content }}
+            </li>
+        @endforeach
+    </ul>
 </body>
 </html>
