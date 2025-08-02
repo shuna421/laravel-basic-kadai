6fe0596f691b566b32ddce83411dfe4afe6f3177
shuna421 <shuna2001@icloud.com>
2025-08-02 14:35:02 +0900

    Add post routes and update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,6 @@ use App\Http\Controllers\PostController;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
 */
 
 Route::get('/', function () {
@@ -23,11 +18,20 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// 認証済みユーザー向けのルート
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // 投稿作成フォームの表示
+    Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+
+    // 投稿データの保存
+    Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
 });
-Route::get('/posts/{id}', [PostController::class, 'show']);
+
+// 投稿詳細ページ（認証不要）
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 
 require __DIR__.'/auth.php';
